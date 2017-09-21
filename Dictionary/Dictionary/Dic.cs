870f8a8655003ac856a5bccd9edcfab398a4c61e
Mohammad ibrar <m.abrar2018@gmail.com>
2017-09-21 01:09:55 -0700

    Dictionary
    Dictionary in Data struction

---
+++ Dictionary/Dictionary/Dic.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Buble_Sort_String
+{
+    class Dic
+    {
+        string[] dataArray;
+        string searchTerm;
+        public void PopulateData(string[] data, string term)
+        {
+            dataArray = data;
+            searchTerm = term;
+        }
+        public string[] BinarySorting(string[] s)
+        {
+            string temp = "";
+            try
+            {
+                for (int i = 1; i < s.Length; i++)
+                {
+                    for (int j = 0; j < s.Length - i; j++)
+                    {
+                        if (s[j].CompareTo(s[j + 1]) > 0)
+                        {
+                            temp = s[j];
+                            s[j] = s[j + 1];
+                            s[j + 1] = temp;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return s;
+        }
+        public string[] GetWords()
+        {
+            string[] array = File.ReadAllLines(@"TempWordFile.txt");
+            return array;
+        }
+        public string BinarySearch(int low, int high)
+        {
+            int mid = (low + high) / 2;
+            int compare = string.Compare(dataArray[mid], searchTerm);
+            if (low > high)
+            {
+                return "Not Found";
+            }
+            if (searchTerm.ToLower() == dataArray[mid].ToLower())
+            {
+                return mid.ToString();
+            }
+            else if (compare > 0)
+            {
+                return BinarySearch(low, mid - 1);
+            }
+            else
+            {
+                return BinarySearch(mid + 1, high);
+            }
+        }
+        public string AddWord(string word, string meaning)
+        {
+            string temp = "\n" + word + ": " + meaning;
+            File.AppendAllText(@"Words.txt", temp);
+            return "Word Added";
+        }
+    }
+}
---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,119 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace Buble_Sort_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dic dic = new Dic();
+        label:
+
+            Console.WriteLine("\t\t\t\tWEL COME");
+            Console.WriteLine("Dictionary");
+            Console.WriteLine("Select your Choice");
+            Console.WriteLine("1. To Search");
+            Console.WriteLine("2. To Enter a new word");
+            Console.WriteLine("3. To get all words of the alphabat");
+            Console.WriteLine("4. To Exit");
+            try
+            {
+                char opt = Convert.ToChar(Console.ReadLine());
+                if (opt == '1')
+                {
+                    Console.WriteLine("Enter Word to Search");
+                    string reqWord = Console.ReadLine();
+                    string[] data = File.ReadAllLines(@"Words.txt");
+                    File.WriteAllText(@"TempWordFile.txt", "");
+                    for (int i = 0; i < data.Length; i++)
+                    {
+                        if (data[i].Substring(0, 1).ToLower() == reqWord.Substring(0, 1).ToLower())
+                        {
+                            File.AppendAllText(@"TempWordFile.txt", data[i] + "\n");
+                        }
+                    }
+                    string[] temp = new string[2];
+                    string[] wordsmeaning = dic.GetWords();
+                    wordsmeaning = dic.BinarySorting(wordsmeaning);
+                    string[] wordsList = new string[wordsmeaning.Length];
+                    for (int i = 0; i < wordsmeaning.Length; i++)
+                    {
+                        temp = wordsmeaning[i].Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+                        wordsList[i] = temp[0];
+                    }
+                    dic.PopulateData(wordsList, reqWord);
+                    string index = dic.BinarySearch(0, wordsList.Length);
+                    if (index != "Not Found")
+                    {
+                        Console.WriteLine("Total Words are: " + wordsList.Length);
+                        Console.WriteLine(wordsmeaning[Convert.ToInt32(index)]);
+                        Console.ReadLine();
+                        Console.Clear();
+                        goto label;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Word Not Found");
+                        Console.ReadLine();
+                        Console.Clear();
+                        goto label;
+                    }
+                }
+                else if (opt == '2')
+                {
+                    Console.WriteLine("Enter Word to Add");
+                    string word = Console.ReadLine();
+                    Console.WriteLine("Enter Meaning");
+                    string meaning = Console.ReadLine();
+                    Console.WriteLine(dic.AddWord(word, meaning));
+                    Console.ReadLine();
+                    Console.Clear();
+                    goto label;
+                }
+                else if (opt == '4')
+                {
+                    Environment.Exit(0);
+                }
+                else if (opt == '3')
+                {
+                    Console.WriteLine("Enter Alphabat to Search");
+                    string reqWord = Console.ReadLine();
+                    string[] data = File.ReadAllLines(@"Words.txt");
+                    File.WriteAllText(@"TempWordFile.txt", "");
+                    int count = 0;
+                    for (int i = 0; i < data.Length; i++)
+                    {
+                        if (data[i].Substring(0, 1).ToLower() == reqWord.Substring(0, 1).ToLower())
+                        {
+                            Console.WriteLine(data[i]);
+                            count++;
+                        }
+                    }
+                    Console.WriteLine("Total Words: " + count);
+                    Console.ReadLine();
+                    goto label;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong Input Try Again");
+                    Console.ReadLine();
+                    Console.Clear();
+                    goto label;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Wrong Input Try Again");
+                Console.ReadLine();
+                Console.Clear();
+                goto label;
+            }
+        }
+    }
+}
+    
+
